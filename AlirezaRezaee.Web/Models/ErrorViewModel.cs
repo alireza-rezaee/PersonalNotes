ed95622801283352e60c09ea7dd4ded6141e20d6
Alireza@rezaee.org <Alireza@rezaee.org>
2020-01-03 20:32:44 +0330

    Add project files.

---
+++ AlirezaRezaee.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AlirezaRezaee.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
