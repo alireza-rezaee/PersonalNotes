1dc69fbcc382face10774ed075f84d029faa48a2
Alireza@rezaee.org <Alireza@rezaee.org>
2020-01-14 12:31:07 +0330

    Simple Changes around Titles and Notes

--- AlirezaRezaee.Web/Startup.cs
+++ AlirezaRezaee.Web/Startup.cs
@@ -29,7 +29,7 @@ namespace AlirezaRezaee.Web
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("LocalDb")));
+                    Configuration.GetConnectionString("AlirezaRezaee")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
