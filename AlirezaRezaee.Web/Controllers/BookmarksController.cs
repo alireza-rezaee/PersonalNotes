e47e685f0a58a004f46d58ef45d2132bc618fc8f
Alireza Rezaee <alireza-git-commits@rezaee.org>
2020-10-03 18:57:38 +0330

    [Feature] Add Bookmarks (NotImplemented)

---
+++ AlirezaRezaee.Web/Controllers/BookmarksController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rezaee.Alireza.Web.Controllers
+{
+    [Route("bookmarks")]
+    public class BookmarksController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
