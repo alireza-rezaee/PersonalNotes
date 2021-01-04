b577f1f65ec4a786e785d789a3891e0dfc166d42
Alireza Rezaee <alireza-git-commits@rezaee.org>
2021-01-04 07:07:04 +0330

    [feature] support NETCOREAPP3_1 target

--- src/Rezaee.Alireza.Web/Areas/Identity/IdentityHostingStartup.cs
+++ src/Rezaee.Alireza.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -2,7 +2,7 @@
 using Rezaee.Alireza.Web.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
+//using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
